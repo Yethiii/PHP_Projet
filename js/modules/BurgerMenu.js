e404dc388ca627623cf6a612f0fd80a080f34350
LaetitiaV <laetitia.voue@gmail.com>
2025-03-01 11:36:10 +0100

    MenuMobileOK

--- js/modules/BurgerMenu.js
+++ js/modules/BurgerMenu.js
@@ -1,40 +1,58 @@
 
-function OuvrirMenu(bouton) {
-    bouton.setAttribute("aria-expanded", "true");
-    bouton.setAttribute("aria-label", "Masquer le menu");
+function OuvrirMenu(menu, bouton) {
+    menu.setAttribute("aria-expanded", "true");
+    menu.style.display = "flex"
+    bouton.setAttribute("aria-label", "Masquer le menu")
 };
 
-function FermerMenu(bouton) {
-    bouton.setAttribute("aria-expanded", "false");
+function FermerMenu(menu, bouton) {
+    menu.setAttribute("aria-expanded", "false");
+    menu.style.display = "none";
     bouton.setAttribute("aria-label", "Ouvrir le menu");
 };
 
-const ClickButton = (e, bouton) => {
+function EstEnMobile() {
+    return window.innerWidth <= 768;
+}
+const ClickButton = (e, bouton, menu) => {
     // Éviter l'action par défaut lorsque l'on clique sur le bouton.
     e.preventDefault();
 
-    // Vérifier l'état du bouton à l'aide de son attribut d'accessibilité "aria-expanded" :
-    const menu = document.querySelector('#menu'); // Récupérer le menu
+    if (!EstEnMobile()) return;
 
     if (menu.getAttribute('aria-expanded') === "false") {
-        OuvrirMenu(menu);
+        OuvrirMenu(menu,bouton);
     } else {
-        FermerMenu(menu);
+        FermerMenu(menu,bouton);
     }
 };
 
-const TouchKey = (e, bouton) => {
+const TouchKey = (e, bouton, menu) => {
+    if (!EstEnMobile()) return;
+
     if (e.key === "Enter" || e.key === " ") {  // " " pour espace
-        ClickButton(e, bouton);
+        ClickButton(e, bouton, menu);
     }
 };
 
 function BurgerMenu(bouton) {
-    FermerMenu(bouton);
+    const menu = document.querySelector('#menu')
 
-    // Initialiser l'évènement déclenché par le clic sur le bouton du menu.
-    bouton.addEventListener('click', (e) => ClickButton(e, bouton));
-    bouton.addEventListener('keydown', (e) => TouchKey(e, bouton));
+    if (EstEnMobile())
+    {FermerMenu(menu, bouton);}
+    else {
+        menu.style.display = "flex";
+    }
+    bouton.addEventListener('click', (e) => ClickButton(e, bouton, menu));
+    bouton.addEventListener('keydown', (e) => TouchKey(e, bouton, menu));
 };
+
+window.addEventListener('resize', () => {
+    if (EstEnModeMobile()) {
+        FermerMenu(menu, bouton);
+    } else {
+        menu.style.display = "flex";
+    }
+});
 
 export { BurgerMenu };
